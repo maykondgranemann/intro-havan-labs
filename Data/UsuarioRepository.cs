cd45e7fdfe9edca7d05d26a8e2ce2dc421eb1755
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-07-13 09:52:16 -0300

    Classe repository

---
+++ Data/UsuarioRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class UsuarioRepository
+    {
+        // Create
+        public void Create(Usuario model)
+        {
+            using (HavanLabsContext context = new HavanLabsContext())
+            {
+                context.Usuarios.Add(model);
+                context.SaveChanges();
+            }
+        }
+        // ReadAll
+        public List<Usuario> ReadAll()
+        {
+            using (HavanLabsContext context = new HavanLabsContext())
+            {
+                return context.Usuarios.ToList();
+            }
+        }
+        // ReadById
+        public Usuario ReadById(int id)
+        {
+            using (HavanLabsContext context = new HavanLabsContext())
+            {
+                return context.Usuarios.Find(id);
+            }
+        }
+        // Update
+        public void Updade(Usuario model)
+        {
+            using (HavanLabsContext context = new HavanLabsContext())
+            {
+                context.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        // Delete
+        public void Delete(int id)
+        {
+            using (HavanLabsContext context = new HavanLabsContext())
+            {
+                Usuario model = context.Usuarios.Find(id);
+                context.Usuarios.Remove(model);
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Web/Controllers/UsuarioController.cs
+++ Web/Controllers/UsuarioController.cs
@@ -9,18 +9,15 @@ namespace Web.Controllers
 {
     public class UsuarioController : Controller
     {
-        // GET: Usuario
+        UsuarioRepository repository = new UsuarioRepository();
+        
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult List()
         {
-            List<Usuario> model = new List<Usuario>();
-            using (var context = new HavanLabsContext())
-            {
-                model = context.Usuarios.ToList();
-            }
+            List<Usuario> model = repository.ReadAll();
             return View(model);
         }
         public ActionResult Create()
@@ -30,41 +27,24 @@ namespace Web.Controllers
         [HttpPost]
         public ActionResult Create(Usuario model)
         {
-            using (var context = new HavanLabsContext())
-            {
-                context.Usuarios.Add(model);
-                context.SaveChanges();
-            }
+            repository.Create(model);
             return RedirectToAction("List");
         }
         public ActionResult Update(int id) 
         {
-            Usuario model = new Usuario();
-            using(var context = new HavanLabsContext())
-            {
-                model = context.Usuarios.Find(id);
-            }
+            Usuario model = repository.ReadById(id);
             return View(model);
         }
         [HttpPost]
         public ActionResult Update(Usuario model)
         {
-            using (var context = new HavanLabsContext())
-            {
-                context.Entry(model).State = System.Data.Entity.EntityState.Modified;
-                context.SaveChanges();
-            }
+            repository.Updade(model);
             return RedirectToAction("List");
         }
 
         public ActionResult Delete(int id)
         {
-            using (var context = new HavanLabsContext())
-            {
-                Usuario model = context.Usuarios.Find(id);
-                context.Usuarios.Remove(model);
-                context.SaveChanges();
-            }
+            repository.Delete(id);
             return RedirectToAction("List");
         }
     }
